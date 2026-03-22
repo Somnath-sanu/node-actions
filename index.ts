024231ca9d5c70392c5271d6c8eb1febcd7a6e88
Somnath Mishra <somnathmishra100dbi@gmail.com>
2026-03-23 00:00:54 +0530

    setup github actions

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res
+    .json({
+      message: "Get request success",
+    })
+    .status(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
